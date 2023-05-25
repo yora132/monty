ab5f7e3c94a864687dc43033087e6df8d0b88cf7
yora132 <yarasalah132@gmail.com>
2023-05-25 08:50:27 -0700

    first

--- operators.c
+++ operators.c
@@ -37,28 +37,3 @@ void f_push(stack_t **head, unsigned int counter)
 	else
 		addqueue(head, i);
 }
-
-
-
-
-/**
-* f_pall - function that prints everything in the stack
-* @head: double head pointer to the stack
-* @counter: unused line count
-*
-* Return: nothing
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-}
